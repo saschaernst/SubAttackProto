04feedaab3fe3dabd552204078b9e5432dbee2f9
Sascha Ernst <mail@saschaernst.de>
2014-12-14 22:57:16 +0100

    control ship by arrow keys

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -5,21 +5,28 @@ public class ShipController : MonoBehaviour
 {
 	public float speed;
 	public float acceleration;
+	public float drag;
+	public float rotSpeed;
 
-	float _targetSpeed = 0f;
+	int _targetSpeed = 0;
+	float _currentSpeed = 0f;
 
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			UpdateSpeed (1);
+			UpdateTargetSpeed (1);
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			UpdateSpeed (-1);
+			UpdateTargetSpeed (-1);
 		}
+
+		UpdateSpeed ();
+		UpdateRotation ();
+		UpdatePosition ();
 	}
 
-	void UpdateSpeed (int dir)
+	void UpdateTargetSpeed (int dir)
 	{
-		var nextSpeed = _targetSpeed + speed * dir;
+		var nextSpeed = _targetSpeed + dir;
 
 		if (nextSpeed < -2) {
 			_targetSpeed = -2;
@@ -30,22 +37,51 @@ public class ShipController : MonoBehaviour
 		}
 	}
 
-	void FixedUpdate ()
+	void UpdateSpeed ()
 	{
-		var delta = _targetSpeed - rigidbody2D.velocity.y;
+		float thrust = Time.deltaTime * acceleration * _targetSpeed;
 
-		if (delta != 0) {
-			var vec = new Vector2 ();
-
-			if (delta > 0) {
-				vec.y = Math.Min (acceleration, delta);
-			} else if (delta < 0) {
-				vec.y = Math.Max (-acceleration, delta);
+		if (_targetSpeed == 0) {
+			if (_currentSpeed < -drag) {
+				_currentSpeed += drag;
+			} else if (_currentSpeed > drag) {
+				_currentSpeed -= drag;
+			} else {
+				_currentSpeed = 0;
 			}
-
-			rigidbody2D.AddForce (vec);
+		} else if (_targetSpeed > 0) {
+			if (_currentSpeed < _targetSpeed - thrust) {
+				_currentSpeed += thrust;
+			} else if (_currentSpeed > _targetSpeed + drag) {
+				_currentSpeed -= drag;
+			} else {
+				_currentSpeed = _targetSpeed;
+			}
+		} else if (_targetSpeed < 0) {
+			if (_currentSpeed + thrust > _targetSpeed) {
+				_currentSpeed += thrust;
+			} else if (_currentSpeed + drag < _targetSpeed) {
+				_currentSpeed += drag;
+			} else {
+				_currentSpeed = _targetSpeed;
+			}
 		}
+	}
 
-		Debug.Log (rigidbody2D.velocity + " " + delta);
+	void UpdateRotation ()
+	{
+		if (_currentSpeed != 0) {
+			var dir = _currentSpeed / Math.Abs (_currentSpeed);
+			var deltaR = Input.GetAxis ("Horizontal") * rotSpeed * dir * Time.deltaTime;
+			transform.Rotate (new Vector3 (0, 0, -deltaR));
+		}
+	}
+
+	void UpdatePosition ()
+	{
+		Vector3 velocity = new Vector3 (0, _currentSpeed * speed, 0);
+		Vector3 pos = transform.position;
+		pos += transform.rotation * velocity;
+		transform.position = pos;
 	}
 }
